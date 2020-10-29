27858c59127e5aa1a9f24e829531d373eb42ec4d
unknown <abdul.daim26@gmail.com>
2020-10-29 11:18:45 +0000

    new commit;

--- client/src/Pages/register.js
+++ client/src/Pages/register.js
@@ -1,10 +1,43 @@
+import Axios from 'axios';
 import React from 'react'
+import axios from 'axios';
 
-export default function Register() {
-    return (
-        <div>
-        <h1>This is register components</h1>
-            
-        </div>
-    )
+export default class Register extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            data : []
+        }
+    }
+
+    componentDidMount(){
+        var url = "https://cat-fact.herokuapp.com/facts";
+        axios.get(url).then((response)=>{
+            this.setState({
+                data : response.data.all
+            })
+        }).then((error)=>{
+            console.log(error);
+        })
+    }
+
+
+    render(){
+        return (
+            <div className="row">
+            <br/>
+
+            {this.state.data.map((item)=>{
+                return <div className="col-3"> <div className="jumbotron bg-dark">{item.text}</div> </div>
+            })}
+           
+
+                
+            </div>
+        )
+    }
+ 
+
 }
+
+
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './navbar.css';
 
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-white">
-            <a class="navbar-brand" href="/">JobServe</a>
+            <nav class="navbar navbar-expand-lg navbar-light bg-white bg-no">
+            <a class="navbar-brand " href="/">JobServe</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -14,14 +15,14 @@ export default function Navbar() {
                     <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
+                    <a class="nav-link " href="/login">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
+                    <a class="nav-link " href="/register">Register</a>
                 </li>
              
                 </ul>
-            </div>
+          </div>
             </nav>
         </div>
     )
